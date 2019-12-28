d2dd745669229499275e4bcbe4ba788bdd3e60df
Cho Chi Him <chochihim@gmail.com>
2019-12-28 17:50:11 +0800

    temp: fix ts error

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -184,3 +184,5 @@ export interface Track extends TrackMetadataBase {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any
 }
+
+export interface TrackMetadata extends TrackMetadataBase {}
